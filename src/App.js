40503acc0c8803b71e50aaff30ea311397bbcff0
Jcheck27 <jordan.checkoff@gmail.com>
2022-07-19 23:20:25 -0500

    Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import Navbar from './components/Navbar';
 
+const name = "Checkoff Design";
+
+const pages = [{
+              name: "a",
+              link: "http://www.google.com"
+              },
+            {
+              name: "b",
+              link: "http://www.wikipedia.com"
+            }]
+
+const logo = "https://common.northwestern.edu/v8/css/images/northwestern.svg";
+
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navbar name={name} links={pages} spread={"separate"} logo={logo}/>
       <div className={'strip'}>
       <h1>My h1</h1>
       <p>My pfjaslk;dfjlkasdjfklasdjf kld;sfj aksdjf; My pfjaslk;dfjlkasdjfklasdjf kld;sfj aksdjf;My pfjaslk;dfjlkasdjfklasdjf 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,23 @@
 const Navbar = (props) => {
 
-    // const styles = {
-    //     nav: {
-    //         backgroundColor: props.bg
-    //     }
-    // }
+    const styles = {
+        nav: {
+            justifyContent: (props.spread == 'separate') ? 'space-between'
+                            : (props.spread == 'left') ? 'flex-start'
+                            : (props.spread == 'center') ? 'space-around' : 'space-between',
+            flexDirecton: "row",
+        }
+    }
 
     return (
-        <nav className={"container test"}>
-            <p>test</p>
-            {props.children}
+        <nav style={styles.nav} className={props.spread == 'center' ? "row" : '' }>
+            <p>
+                <span className={props.logo ? 'sr-only' : ''}>{props.name}</span>
+                {props.logo ? <img src={props.logo} /> : ''}
+            </p>
+            <ul>
+                {props.links.map((page) => <a href={page.link}><li>{page.name}</li></a>)}
+            </ul>
         </nav>
     )
 }
